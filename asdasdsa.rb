31a43537fa33f93daf22973206457c51d4156761
Rafael Vigo <rvigo07+github@gmail.com>
2024-02-12 16:01:16 -0300

    Create asdasdsa.rb

---
+++ asdasdsa.rb
@@ -0,0 +1,45 @@
+# typed: false
+# frozen_string_literal: true
+
+class asdasdsa < Formula
+  homepage "https://github.com/rvigo/asdasdsa"
+  version "0.0.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/rvigo/asdasdsa/releases/download/v0.0.0/asdasdsa_0.0.0_aarch64-apple-darwin.tar.gz"
+      sha256 ""
+
+      def install
+       bin.install "asdasdsa"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/rvigo/asdasdsa/releases/download/v0.0.0/asdasdsa_0.0.0_x86_64-apple-darwin.tar.gz"
+      sha256 ""
+
+      def install
+        bin.install "asdasdsa"
+      end
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/rvigo/asdasdsa/releases/download/v0.0.0/asdasdsa_0.0.0_aarch64-unknown-linux-gnu.tar.gz"
+      sha256 ""
+
+      def install
+        bin.install "asdasdsa"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/rvigo/asdasdsa/releases/download/v0.0.0/asdasdsa_0.0.0_x86_64-unknown-linux-gnu.tar.gz"
+      sha256 ""
+
+      def install
+        bin.install "asdasdsa"
+      end
+    end
+  end
+end
