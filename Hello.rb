ff105bbe0307d028587b34a9236873155ac8e088
Rafael Vigo <rvigo07+github@gmail.com>
2024-02-25 00:18:42 -0300

    Bump version to v9.9.9

--- Hello.rb
+++ Hello.rb
@@ -8,8 +8,8 @@ class  < Formula
     homepage "https://example.com"
     version "v9.9.9"
     license "MIT"
-    url ""
-    hash ""
+    url "https://github.com/rvigo/asdasdsa/releases/download/vv9.9.9/rustreleaser_v9.9.9.tar.gz"
+    hash "98ff72af8d8aa67c7299be128ae714c4e017dfc6c5b6201d0f7b9cb9e6f0dd7a"
 
     def install
         "This is a test formula"
