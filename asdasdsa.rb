0d313db98d56213b0bdabe3e9911fcab0037bcd0
Rafael Vigo <19755627+rvigo@users.noreply.github.com>
2024-06-21 23:25:13 -0300

    update formula

--- asdasdsa.rb
+++ asdasdsa.rb
@@ -11,12 +11,6 @@ class Asdasdsa < Formula
   license "MIT"
   head "https://github.com/rvigo/asdasdsa.git", branch: "main"
 
-  bottle do
-    root_url "https://github.com/rvigo/homebrew-asdasdsa/releases/download/asdasdsa-1.2.1"
-    sha256 cellar: :any_skip_relocation, ventura:      "2df41bceb06f3aa314a2d14b4f21a6f69f28a26be239afa54c2bc51b9d4c47bb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5c71b3020d08416a8c2ab019f6d69505a7171c803e2be8ba4c48cbd6bb3685ea"
-  end
-
   depends_on "rust" => :build
 
   def install
